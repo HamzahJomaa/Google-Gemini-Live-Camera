7baf7708d2a3ec27c444055a72c1900ce4513a27
Emir HAMURCU <sentrycoder.dev0@gmail.com>
2024-01-11 23:27:37 +0300

    The problem of constantly refreshing the camera has been fixed and theme changing has been added.

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from content_description import ContentDescriber
 root = tk.Tk()
 root.title("Webcam Stream")
 
-user_input = tk.Entry(root, width=50)
-user_input.pack()
+root.tk_setPalette(background="#85001B", foreground="#FFFFFF")
+root.option_add("*TButton*highlightBackground", "#2C2C2C")
+root.option_add("*TButton*highlightColor", "#2C2C2C")
+root.option_add("*TButton*foreground", "#FFFFFF")
 
 canvas = tk.Canvas(root, width=640, height=480)
 canvas.pack()
+canvas.config(bg="#2C2C2C", highlightthickness=0)
+
+user_input = tk.Entry(root, width=50)
+user_input.pack()
 
 video_handler = VideoStreamHandler(root, canvas)
 content_describer = ContentDescriber(root, user_input, video_handler)
--- video_stream.py
+++ video_stream.py
@@ -1,5 +1,4 @@
 import cv2
-import threading
 from PIL import Image, ImageTk
 import tkinter as tk
 
@@ -11,29 +10,43 @@ class VideoStreamHandler:
         self.cap = None
         self.photo = None
         self.current_frame = None
+        self.after_id = None
 
     def start_stream(self, camera_index):
         if self.cap is not None:
             self.cap.release()
         self.cap = cv2.VideoCapture(int(camera_index))
-        thread = threading.Thread(target=self.video_stream)
-        thread.start()
+        self.update_video_stream()
+        self.after_id = self.root.after(10, self.video_stream)
 
     def video_stream(self):
-        while self.cap.isOpened():
-            ret, frame = self.cap.read()
-            if ret:
-                self.current_frame = frame
-                cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                img = Image.fromarray(cv2image)
+        ret, frame = self.cap.read()
+        if ret:
+            self.current_frame = frame
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2image)
+
+            if self.photo:
+                self.photo.paste(img)
+            else:
                 self.photo = ImageTk.PhotoImage(image=img)
                 self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-                self.root.update()
+
+            self.after_id = self.root.after(10, self.video_stream)
+
+    def update_video_stream(self):
+        ret, frame = self.cap.read()
+        if ret:
+            self.current_frame = frame
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2image)
+            self.photo = ImageTk.PhotoImage(image=img)
+            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
     def stop_video(self):
         if self.cap is not None and self.cap.isOpened():
             self.cap.release()
-        self.root.destroy()
+        self.root.after_cancel(self.after_id)
 
     def get_current_frame(self):
         return self.current_frame
